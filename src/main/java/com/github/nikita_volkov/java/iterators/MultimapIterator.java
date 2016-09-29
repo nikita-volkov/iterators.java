2074ff3b5c9b8502917b791d2db3aab349700e29
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-09-29 15:11:42 +0300

    Update MultimapIterator

--- src/main/java/com/github/nikita_volkov/java/iterators/MultimapIterator.java
+++ src/main/java/com/github/nikita_volkov/java/iterators/MultimapIterator.java
@@ -2,20 +2,20 @@ package com.github.nikita_volkov.java.iterators;
 
 import java.util.*;
 
-public final class MultimapIterator<key, value> implements Iterator<MultimapIterator.Entry> {
+public final class MultimapIterator<key, value, valueIterable extends Iterable<value>> implements Iterator<MultimapIterator.Entry<key, value>> {
 
-  private final Iterator<Map.Entry<key, Iterable<value>>> iterator;
+  private final Iterator<Map.Entry<key, valueIterable>> iterator;
   private key currentKey;
   private Iterator<value> currentDeeperIterator;
 
-  public MultimapIterator(Map<key, Iterable<value>> map) {
+  public MultimapIterator(Map<key, valueIterable> map) {
     iterator = map.entrySet().iterator();
     fetchNextEntry();
   }
 
   private void fetchNextEntry() {
     if (iterator.hasNext()) {
-      Map.Entry<key, Iterable<value>> entry = iterator.next();
+      Map.Entry<key, valueIterable> entry = iterator.next();
       currentDeeperIterator = entry.getValue().iterator();
       if (!currentDeeperIterator.hasNext()) {
         fetchNextEntry();
@@ -31,14 +31,14 @@ public final class MultimapIterator<key, value> implements Iterator<MultimapIter
   }
 
   @Override
-  public Entry next() {
+  public Entry<key, value> next() {
     key key = currentKey;
     value value = currentDeeperIterator.next();
     fetchNextEntry();
-    return new Entry(key, value);
+    return new Entry<>(key, value);
   }
 
-  public final class Entry {
+  public final static class Entry<key, value> {
     public final key key;
     public final value value;
 
