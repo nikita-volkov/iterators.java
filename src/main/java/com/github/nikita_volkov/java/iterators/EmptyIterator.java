2a3011e3e2ae37bd0605abe15dc3ef27f920678e
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-08-02 01:57:24 +0300

    Correct Empty Iterator

--- src/main/java/com/github/nikita_volkov/java/iterators/EmptyIterator.java
+++ src/main/java/com/github/nikita_volkov/java/iterators/EmptyIterator.java
@@ -1,6 +1,6 @@
 package com.github.nikita_volkov.java.iterators;
 
-import java.util.Iterator;
+import java.util.*;
 
 public final class EmptyIterator<a> implements Iterator<a> {
   @Override
@@ -10,6 +10,6 @@ public final class EmptyIterator<a> implements Iterator<a> {
 
   @Override
   public a next() {
-    return null;
+    throw new NoSuchElementException();
   }
 }
